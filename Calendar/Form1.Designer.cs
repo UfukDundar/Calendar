b13570271b9afe59061daa7516f7c9fd0290c940
ufuk <ufuk-dundar@outlook.com>
2016-12-28 22:28:43 +0300

    Updated

--- Calendar/Form1.Designer.cs
+++ Calendar/Form1.Designer.cs
@@ -28,12 +28,92 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.cmbDay = new System.Windows.Forms.ComboBox();
+            this.cmbMonth = new System.Windows.Forms.ComboBox();
+            this.cmbYear = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // cmbDay
+            // 
+            this.cmbDay.FormattingEnabled = true;
+            this.cmbDay.Location = new System.Drawing.Point(40, 54);
+            this.cmbDay.Name = "cmbDay";
+            this.cmbDay.Size = new System.Drawing.Size(121, 24);
+            this.cmbDay.TabIndex = 0;
+            // 
+            // cmbMonth
+            // 
+            this.cmbMonth.FormattingEnabled = true;
+            this.cmbMonth.Location = new System.Drawing.Point(182, 54);
+            this.cmbMonth.Name = "cmbMonth";
+            this.cmbMonth.Size = new System.Drawing.Size(121, 24);
+            this.cmbMonth.TabIndex = 1;
+            // 
+            // cmbYear
+            // 
+            this.cmbYear.FormattingEnabled = true;
+            this.cmbYear.Location = new System.Drawing.Point(329, 54);
+            this.cmbYear.Name = "cmbYear";
+            this.cmbYear.Size = new System.Drawing.Size(121, 24);
+            this.cmbYear.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(40, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Day";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(179, 34);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Month";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(326, 34);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Year";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(500, 397);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmbYear);
+            this.Controls.Add(this.cmbMonth);
+            this.Controls.Add(this.cmbDay);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ComboBox cmbDay;
+        private System.Windows.Forms.ComboBox cmbMonth;
+        private System.Windows.Forms.ComboBox cmbYear;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- Calendar/Form1.cs
+++ Calendar/Form1.cs
@@ -16,5 +16,38 @@ namespace Calendar
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for(int i=1; i<=31; i++)
+            {
+                cmbDay.Items.Add(i);
+            }
+            List<string> Months = new List<string>();
+            Months.Add("January");
+            Months.Add("February");
+            Months.Add("March");
+            Months.Add("April");
+            Months.Add("May");
+            Months.Add("June");
+            Months.Add("July");
+            Months.Add("August");
+            Months.Add("September");
+            Months.Add("October");
+            Months.Add("November");
+            Months.Add("December");
+
+            int Lenght = Months.Count;
+            for(int monthIndex=0; monthIndex<Lenght; monthIndex++)
+            {
+                cmbMonth.Items.Add(Months[monthIndex]);
+            }
+            int year = DateTime.Today.Year;
+            for (int yearIndex=1930; yearIndex<=year; yearIndex+=4)
+            {
+                cmbYear.Items.Add(yearIndex);
+            }
+
+        }
     }
 }
